aa3b9222108a3babd7eb21a8c05ca010f970f6e0
Hardy, Justin E <151710@s020rmc148-11.local>
2018-12-07 11:18:41 -0600

    Initial Commit

---
+++ Words With Friends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Words With Friends
+//
+//  Created by Hardy, Justin E on 12/7/18.
+//  Copyright © 2018 Hardy, Justin E. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
