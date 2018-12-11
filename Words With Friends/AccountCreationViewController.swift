9a174424ba40ecaa015220fd1de6000a62172a8b
Hardy, Justin E <151710@s020rmc148-11.local>
2018-12-11 11:32:21 -0600

    Added User Interface to the main storyboard, as well as view controllers for each individual view controller.

---
+++ Words With Friends/AccountCreationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AccountCreationViewController.swift
+//  Words With Friends
+//
+//  Created by Hardy, Justin E on 12/7/18.
+//  Copyright © 2018 Hardy, Justin E. All rights reserved.
+//
+
+import UIKit
+
+class AccountCreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+}
---
+++ Words With Friends/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Words With Friends
+//
+//  Created by Hardy, Justin E on 12/7/18.
+//  Copyright © 2018 Hardy, Justin E. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
