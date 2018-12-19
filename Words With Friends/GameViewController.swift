7d5a41ef9e267cb166752de27bf3593060fb0325
Hardy, Justin E <151710@s020rmc148-11.local>
2018-12-19 12:12:09 -0600

    Added bare-bones basic Game UI (very unfinished :/)

---
+++ Words With Friends/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Words With Friends
+//
+//  Created by Hardy, Justin E on 12/19/18.
+//  Copyright © 2018 Hardy, Justin E. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
